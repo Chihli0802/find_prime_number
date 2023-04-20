c1f6296b83db679f8cd452a3e2b6a7917e6021c6
Chihli0802 <annannwhat@gmail.com>
2023-04-20 09:45:47 +0800

    prime

---
+++ main.js
@@ -0,0 +1,34 @@
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
+const answer = document.querySelector("#answer");
+// const num = parseInt(input.value); //輸入的數字值
+const num = input.value; //輸入的數字值
+
+
+
+btn.addEventListener("click", function () {
+    // console.log(input.value);
+    let arr = [];
+    for (let i = 2; i <= input.value; i++) {
+        for (let k=2; k <= i; k++) {
+            if(i===k && i % k === 0 ){
+                arr.push(i);
+            }else if(i%k===0 && i!==k){
+                break;
+            }
+            
+        }
+    }
+    
+
+    let result = "";
+    arr.forEach(function (i) {
+        result += `<span class="mx-1">${i}</span>`
+        console.log(arr);
+    })
+    answer.innerHTML = result;
+
+})
+
+
+
